db4dadb7a9d7dcd880888f3a97254049c32ff30e
oliviergerbe <oliviergerbe@localhost>
2023-03-18 16:06:47 -0400

    specific

---
+++ WebContent/application/specific.js
@@ -0,0 +1,30 @@
+document.write("<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>");
+
+//------ EXEC BATCH AT USER CREATION ------------------
+var g_execbatch = true;
+var g_execbatchbuttonlabel1 = [];
+	g_execbatchbuttonlabel1['fr'] = "Patience! Création de votre portfolio ...";
+var g_json = {};
+
+//=======================
+function prepareBatch()
+//=======================
+{
+	var today=new Date();
+	var annee = today.getFullYear();
+	var mois = today.getMonth() + 1;
+	if (mois<10)
+		mois = "0"+mois;
+	// ---- global variables ---------
+	g_json['model_code'] = "modeles-cnam.batch-creation";
+	g_json['diploma_begin'] = annee+"-"+mois;
+	// ---- local variables ---------
+	g_json['lines'] = [];
+	g_json.lines[0] =
+	{
+		"auditeurUsername" : USER.username,
+		"auditeurCourriel" : USER.email,
+		"auditeurNomFamille" : USER.lastname,
+		"auditeurPrenom" : USER.firstname,
+	};
+}//----------------------------------------------------
